c7583715d8def168a4317760e84b7598f01ad5a5
Aftab Shaikh <44019928+affishaikh@users.noreply.github.com>
2018-12-26 12:19:58 +0530

    Introduced a main function in wc.js
    Extracted readFile and getCount function from wc function in lib.js
    Added a one more test for countCharacters

--- src/lib.js
+++ src/lib.js
@@ -1,10 +1,25 @@
 const ENCODING = 'utf8';
 const NEWLINE = '\n';
+const EMPTYSTRING = '';
+
+const getCount = function(delimeter, fileContents) {
+  return fileContents.split(delimeter).length;
+};
+
+const countLines = getCount.bind(null, NEWLINE);
+const countCharacters = getCount.bind(null, EMPTYSTRING);
+
+const readFile = function(fileName, fs) {
+  return fs.readFileSync(fileName, ENCODING);
+};
 
 const wc = function(prerequisites, fs) {
-    let [option, fileName] = prerequisites;
-    let fileContents = fs.readFileSync(fileName, ENCODING);
-    return fileContents.split(NEWLINE).length;
-}
+  let [option, fileName] = prerequisites;
+  let fileContents = readFile(fileName, fs);
+  if (option === '-l') {
+      return countLines(fileContents);
+  }
+  return countCharacters(fileContents);
+};
 
 module.exports = { wc };
--- test/libTest.js
+++ test/libTest.js
@@ -1,23 +1,37 @@
-const {wc} = require('../src/lib.js');
+const { wc } = require('../src/lib.js');
 const assert = require('assert');
 const ENCODING = 'utf8';
 
 const createReader = function(expectedFileName, fileContents) {
-    return function(actualFileName, actualEncoding) {
-        if(actualFileName === expectedFileName && actualEncoding === ENCODING) {
-            return fileContents;
-        }
+  return function(actualFileName, actualEncoding) {
+    if (actualFileName === expectedFileName && actualEncoding === ENCODING) {
+      return fileContents;
     }
-} 
+  };
+};
 
-describe('wc', function(){
+describe('wc', function() {
+  describe('count lines', function() {
     const fs = {};
-    it('should return number of lines', function(){
-        const fileName = 'numbers';
-        const fileContents = '1\n2\n3';
-        fs.readFileSync = createReader(fileName, fileContents);
-        let expectedOutput = 3;
-        let actualOutput = wc(['-l', 'numbers'], fs);
-        assert.equal(actualOutput, expectedOutput);
+    it('should return number of lines', function() {
+      const fileName = 'numbers';
+      const fileContents = '1\n2\n3';
+      fs.readFileSync = createReader(fileName, fileContents);
+      let expectedOutput = 3;
+      let actualOutput = wc(['-l', 'numbers'], fs);
+      assert.equal(actualOutput, expectedOutput);
     });
+  });
+
+  describe('count characters', function() {
+    const fs = {};
+    it('should return number of characters', function() {
+      const fileName = 'numbers';
+      const fileContents = '1\n2\n3';
+      fs.readFileSync = createReader(fileName, fileContents);
+      let expectedOutput = 5;
+      let actualOutput = wc(['-c', 'numbers'], fs);
+      assert.equal(actualOutput, expectedOutput);
+    });
+  });
 });
